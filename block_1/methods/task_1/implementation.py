e2e3ef65313e62b8f40345ddcc2a770b5c80cf74
Маркелов Родион <r.ziuzev@bars.group>
2024-06-18 23:02:24 +0300

    Завершены задания builtin_functions (task1) и methods (task1)

--- block_1/methods/task_1/implementation.py
+++ block_1/methods/task_1/implementation.py
@@ -1,2 +1,25 @@
 class Coffee:
-    pass
+    coffee = {
+        'капучино': ['эспрессо', 'молоко'],
+        'латте': ['эспрессо', 'молоко', 'молочная пена'],
+        'глясе': ['эспрессо', 'мороженое']
+    }
+
+    def get_cappuccino(self):
+        return self.coffee['капучино']
+
+    def get_latte(self):
+        return self.coffee['латте']
+
+    def get_glace(self):
+        return self.coffee['глясе']
+
+
+def print_result():
+    coffee = Coffee()
+    print(f"Заказываем капучино: " + ' + '.join(coffee.get_cappuccino()))
+    print(f"Заказываем латте: " + ' + '.join(coffee.get_latte()))
+    print(f"Заказываем глясе: " + ' + '.join(coffee.get_glace()))
+
+
+print_result()
